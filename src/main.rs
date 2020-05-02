9dcc66cffd5ba2c44a844ae1c50e40c3b46db5a8
jibuntu <i.am.tomotatu@gmail.com>
2020-05-02 11:07:48 +0900

    コマンドがテスト用のxmlを出力するように変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::BufReader;
 use std::env;
 
 mod tokenizer;
@@ -5,8 +7,18 @@ use tokenizer::Tokenizer;
 
 
 fn main() {
-    let filename = match env::args().nth(1) {
-        Some(f) => f,
+    let f = match env::args().nth(1) {
+        Some(f) => match File::open(f) {
+            Ok(f) => f,
+            Err(_) => return println!("ファイルが開けません")
+        }
         None => return println!("ファイル名を指定してください")
     };
+
+    let reader = BufReader::new(f);
+    println!("<tokens>");
+    for token in Tokenizer::new(reader) {
+        println!("  {}", token.to_xml());
+    }
+    println!("</tokens>");
 }
