2ade57cadfc133f99c6441c9a9e49cd41e50b538
jibuntu <i.am.tomotatu@gmail.com>
2020-09-28 15:51:29 +0900

    テストを部分ごとに分割した

--- test/test_compilation_engine.sh
+++ test/test_compilation_engine.sh
@@ -3,12 +3,35 @@
 compiler="../target/debug/compiler"
 comparer="../../../../tools/TextComparer.sh"
 
+
 function test () {
-    printf $1": "
-    $compiler $1 out.xml
-    sh $comparer $2 out.xml
+    echo $1": "
+    compile_result=$($compiler $1 out.xml)
+    result=$(sh $comparer $2 out.xml)
+
+    printf "\x1b[31m" # 文字を赤色にする
+    if [[ $result == *"Comparison ended successfully"* ]]; then
+        printf "\x1b[32m" # 文字を黄緑色にする
+    fi
+
+    if [[ $compile_result != "" ]]; then
+        echo "    "$compile_result
+    fi
+
+    echo "    "$result
+    printf "\x1b[0m" # 文字色を戻す
     rm out.xml
 }
 
+function t () {
+    test "xml/"$1.jack "xml/"$1.xml
+}
+
+
 cargo build
-test xml/test.jack xml/test.xml
+
+t "test"
+t "class"
+t "class_var_dec"
+t "subroutine_dec"
+t "var_dec"
